23f2bb092979352fc23ac73e7c56b917ab1816d8
zcj <talenterj@gmail.com>
2021-07-20 15:07:39 +0800

    /Users/talenter/project/TRIAD/utilities/persistent_cache/hash_table_evictable.h

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:bf95aecd135cb495298554d4cc5d17126810bb6f";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:462567b6f1a2f2e3934d8c4b093f09455e1cc494";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2021-07-20";
 const char* rocksdb_build_compile_date = __DATE__;
